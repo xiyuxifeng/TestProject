58f3efa093f15da8cd0f33f441b705393bb58cf2
wanghui <wang.hui@aldelo.com>
2022-09-26 17:30:35 +0800

    test

--- MainAppTests/MainAppTests.swift
+++ MainAppTests/MainAppTests.swift
@@ -25,6 +25,14 @@ final class MainAppTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
+    
+    func testApp() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
