625d9b380fe9f156993bb55016dc168c17ab5518
xiyuxifeng <xiyuxifeng@163.com>
2022-09-29 22:29:58 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
 date = $1
-git log  --no-merges --after=$1 --date=format:'%Y-%m-%d' --pretty=format:'【%an】%d %s'  > log.txt
+git log  --no-merges --after='$date' --date=format:'%Y-%m-%d' --pretty=format:'【%an】%d %s' > log.txt
