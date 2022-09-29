2e380a5a6016afd27f3dea019c139967bdeedd6a
xiyuxifeng <xiyuxifeng@163.com>
2022-09-29 23:12:37 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,5 @@
 # !/bin/bash
 
-lastDate = $1
+lastDate=$1
 echo $lastDate
-echo "--after='${date}'"
-git log  --no-merges --after='${date}' --date=format:'%Y-%m-%d' --pretty=format:'【%an】%d %s' > log.txt
+git log --no-merges --after=${lastDate} --date=format:'%Y-%m-%d' --pretty=format:'【%an】%d %s' > log.txt
