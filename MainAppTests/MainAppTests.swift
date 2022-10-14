4c150994ee4bb6c70344fc05b27d8f0764af1c95
xiyuxifeng <xiyuxifeng@163.com>
2022-10-14 16:47:05 +0800

    Update MainAppTests.swift

--- MainAppTests/MainAppTests.swift
+++ MainAppTests/MainAppTests.swift
@@ -41,7 +41,7 @@ final class MainAppTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        XCTAssertEqual(customeSum(1,2,3), 7)
+        XCTAssertEqual(customeSum(1,2,3), 8)
     }
 
     func testPerformanceExample() throws {
